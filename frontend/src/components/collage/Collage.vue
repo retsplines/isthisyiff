dd6855a8bdbdda19579ef25a412fc205cedd562b
Ret <folf@retsplin.es>
2023-03-26 19:32:01 +0100

    Fix: Easing of the collage drift taking too long

--- frontend/src/components/collage/Collage.vue
+++ frontend/src/components/collage/Collage.vue
@@ -361,7 +361,8 @@ function update(now: DOMHighResTimeStamp) {
 
     // Cubically reduce the drift rate
     if (driftRate.x !== 0 || driftRate.y !== 0) {
-        driftRate = functionOf(driftRate, d => (d < 1 && d > -1) ? 0 : (d * 0.95));
+        driftRate = functionOf(driftRate, d => Math.floor(Math.abs(d) < 25 ? 0 : d * 0.95));
+        console.log(driftRate);
     }
 
     // Shift an appropriate amount for the passed time
